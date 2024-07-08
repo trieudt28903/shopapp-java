3f173a2ce579f35e9501d09ae14128a82463ac2b
le dinh trieu <trieu28903@gmail.com>
2024-07-09 00:46:41 +0700

    up user

--- demo/src/main/java/com/dev/shopdienthoai/demo/controller/UserController.java
+++ demo/src/main/java/com/dev/shopdienthoai/demo/controller/UserController.java
@@ -1,23 +1,31 @@
 package com.dev.shopdienthoai.demo.controller;
 
 
+import com.dev.shopdienthoai.demo.domain.User;
 import com.dev.shopdienthoai.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
-    public UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
-    public String getHomePage(){
-        return userService.handleHello();
+    @GetMapping("/user/create")
+    public String createNewUser(){
+        User user = new User();
+        user.setEmail("trieu@gmail.com");
+        user.setPassword("123456");
+        user.setName("trieudeptrai");
+        this.userService.saveUser(user);
+
+
+        return "tạo user thành công";
     }
 }
--- demo/src/main/java/com/dev/shopdienthoai/demo/service/UserService.java
+++ demo/src/main/java/com/dev/shopdienthoai/demo/service/UserService.java
@@ -1,10 +1,15 @@
 package com.dev.shopdienthoai.demo.service;
 
+import com.dev.shopdienthoai.demo.domain.User;
+import com.dev.shopdienthoai.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public String handleHello(){
-        return "Hello from UserService!";
+    @Autowired
+    private UserRepository userRepository;
+    public void saveUser(User user){
+         userRepository.save(user);
     }
 }
